29648a05e4fad05a5111718955709b1c7338a59c
Charles Ivia <iviacharles@gmail.com>
2020-07-14 16:58:05 +0300

    find_33 fn

---
+++ Functions/find_33.py
@@ -0,0 +1,13 @@
+# FIND 33:
+# Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+
+
+def has_33(nums):
+    for n in range(0, len(nums) - 1):
+        if nums[n] == 3 and nums[n+1] == 3:
+            return True
+    return False
+
+print(has_33([1, 3, 3]))
+print(has_33([1, 3, 1, 3]))
+print(has_33([3, 1, 3]))
