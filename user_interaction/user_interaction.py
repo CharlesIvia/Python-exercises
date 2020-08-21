3e043800da9368353390d2eb5c23a58ca0ded7c8
Charles Ivia <iviacharles@gmail.com>
2020-08-21 16:14:31 +0300

    Logic to check position choice

--- user_interaction/user_interaction.py
+++ user_interaction/user_interaction.py
@@ -2,3 +2,14 @@
 def display_game(game_list):
     print("Here is the current list")
     print(game_list)
+
+#Choose position
+
+
+def position_choice():
+    choice = "wrong"
+    while choice not in ["0", "1", "2"]:
+        choice = input("Pick a position (0,1,2)")
+        if choice not in ["0", "1", "2"]:
+            print("Sorry, invalid choice")
+    return int(choice)
