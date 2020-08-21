668bbc99b2db5f89819072f7561ccfbce670b708
Charles Ivia <iviacharles@gmail.com>
2020-08-21 16:18:03 +0300

    Initialize user interaaction

--- user_interaction/user_interaction.py
+++ user_interaction/user_interaction.py
@@ -37,3 +37,14 @@ def gameon_choice():
         return True
     else:
         return False
+
+#Initialize game
+game_on = True
+game_list = [0, 1, 2]
+while game_on:
+    display_game(game_list)
+    position = position_choice()
+    game_list = replacement_choice(game_list, position)
+    display_game(game_list)
+
+    game_on = gameon_choice()
